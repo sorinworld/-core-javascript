72a8fc4999c2c40d06bcf30805e2624d3c92b060
sorinworld <jsorin1123@gmail.com>
2023-01-20 19:55:17 +0900

    [add]typeConversion 학습

--- README.md
+++ README.md
@@ -11,5 +11,5 @@
 - [x] Code Structure [[바로가기]](https://github.com/sorinworld/-core-javascript/blob/01.core/client/chapter/core/01.codeStructure.js)
 - [x] Variables [[바로가기]](https://github.com/sorinworld/-core-javascript/blob/01.core/client/chapter/core/02.variables.js)
 - [x] Strict Mode [[바로가기]](https://github.com/sorinworld/-core-javascript/blob/01.core/client/chapter/core/03.strict.js)
-- [x] Global This
-- [x] Data Types
+- [x] Global This [[바로가기]](https://github.com/sorinworld/-core-javascript/blob/01.core/client/chapter/core/04.globalThis.js)
+- [x] Data Types [[바로가기]](https://github.com/sorinworld/-core-javascript/blob/01.core/client/chapter/core/05.dataType.js)
---
+++ client/chapter/core/06.typeConversion.js
@@ -0,0 +1,68 @@
+/* ---------------------------------------------------------------------- */
+/* Type Conversion                                                        */
+/* ---------------------------------------------------------------------- */
+
+const user = {
+  sum : function (){
+      return this
+    }
+}
+
+console.log(user.sum());
+
+
+/* 데이터 → 문자 ----------------------------------------------------------- */
+
+// string() / 'string' + ''
+// number
+//number -> '2022'
+const YEAR = 2022;
+
+console.log(String(YEAR)); //2022
+
+
+// undefined -> 'undefined'
+// null -> 'null'
+
+let days = null;
+console.log(String(days));
+
+// boolean -> 'true' or 'false'
+
+let isKind = true;
+console.log(isKind + '');
+
+/* 데이터 → 숫자 ----------------------------------------------------------- */
+
+// Number()
+
+// undefined -> NaN
+console.log(Number(undefined));
+
+// null -> 0
+let money = null;
+console.log(Number(money));
+
+// boolean / true : 1 , false : 0
+// *1
+// /1
+// +
+let cutie = false;
+console.log(+cutie);
+
+// string 숫자형 문자
+let num = '   123   '
+console.log(num * 1);
+
+// numeric string
+let width = '32.23120px';
+console.log(parseFloat(width,10));
+
+
+/* 데이터 → 불리언 ---------------------------------------------------------- */
+
+// null, undefined, 0, NaN, ''
+// 위에 나열한 것 이외의 것들 
+
+console.log(Boolean(123));
+console.log(!!null);
