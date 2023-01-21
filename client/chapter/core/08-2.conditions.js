ed655a481009047c58d24949546f40ec08970c88
sorinworld <jsorin1123@gmail.com>
2023-01-21 18:08:28 +0900

    [add] 논리연산자 학습

---
+++ client/chapter/core/08-2.conditions.js
@@ -0,0 +1,46 @@
+// if(age >= 14 && age <= 90)
+// if(!(age >= 14 && age <= 90))
+// if(age < 14 || age > 90 )
+
+let age = 20;
+
+if (age >= 14 && age <= 90) {
+}
+
+let a = 10;
+let b = '';
+let value = Boolean(b);
+
+// 논리곱(그리고) 연산자
+let AandB = a && b;
+
+// 논리합(또는) 연산자
+let AorB = a || b;
+
+// 부정 연산자
+let reverseValue = !value;
+
+// 조건 처리
+
+// 첫번째 Falsy를 찾는 연산 (&&)
+let whichFalsy = true && ' ' && [] && { thisIsFalsy: false };
+
+// 첫번째 Truthy를 찾는 연산 (||)
+let whichTruthy = false || '' || [2, 3].length || { thisIsTruthy: true };
+
+
+//logIn
+
+let userName = prompt('사용자 아이디를 입력해주세요','');
+if(userName?.toLowerCase() === 'admin'){
+  let pw = prompt('비밀번호를 입력하세요','');
+  if(pw?.toLowerCase() === 'TheMaster'){
+    alert('환영합니다');
+  } else {
+    alert('인증에 실패하였습니다')
+  }
+} else if(userName.replace(/\s*/g,'') ===  '' || userName === null){ //.replace(/\s*/g,'' 전체 공백제거
+  alert('취소되었습니다.');
+} else {
+  alert('인증에 실패하였습니다.');
+}
